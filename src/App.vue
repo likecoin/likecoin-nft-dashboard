282b8bc2540d8efad6fd9f3bd007699686d9e62f
William Chong <me@williamchong.cloud>
2022-11-02 13:15:42 +0800

    💄 Add header style and info link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1>LikeCoin NFT Dashboard</h1>
-  <div>
+  <header>
+    <h1 class="gradient-text">LikeCoin NFT Dashboard</h1>
+  </header>
+  <div style="margin: 10px">
     <div>Select a page</div>
     <div>
       <span
@@ -14,6 +16,10 @@
       <router-view />
     </div>
   </div>
+  <footer>
+    <a href="https://liker.land">Writing NFT</a>
+    <a href="https://like.co">About LikeCoin</a>
+  </footer>
 </template>
 
 <script setup>
@@ -29,19 +35,54 @@ function selectPage(n) {
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
+
+header {
+  background-color: #28646e;
+  width: 100%;
+  height: 100px;
+  margin: 0;
+}
+
+.gradient-text {
+  margin: 0;
+  padding-top: 25px;
+  display: flex;
+  justify-content: center;
+  color: transparent;
+  font-weight: 600;
+  font-size: 36px;
+  background-clip: text;
+  -webkit-background-clip: text;
+  --tw-gradient-to: #f0e6b4;
+  --tw-gradient-from: #d2f0f0;
+  --tw-gradient-stops: var(--tw-gradient-from),
+    var(--tw-gradient-to, rgba(210, 240, 240, 0));
+  background-image: linear-gradient(to right, var(--tw-gradient-stops));
+}
+
 table {
   width: 100%;
   border-collapse: collapse;
 }
 table, th, td {
   border: 1px solid;
+}
+
+footer a {
+  margin-left: 10px;
 }
 </style>
