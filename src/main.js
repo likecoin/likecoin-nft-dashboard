bef1d771cc6893e96e8b1a3879bc0e802def7296
William Chong <me@williamchong.cloud>
2022-11-05 21:21:03 +0800

    📈 Add gtag

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
+import VueGtag from 'vue-gtag';
 import { router } from './router';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
+app.use(VueGtag, {
+  config: { id: 'G-WZNJHM0MM1' },
+}, router);
 app.mount('#app');
