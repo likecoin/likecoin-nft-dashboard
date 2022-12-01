9bed07ce339c19e4b3f4cc380cc2ece49eec1c62
Wei-Jun Hung <weijunhung@outlook.com>
2022-12-01 14:31:21 +0800

    ♻️ Simplify fetching userInfo check

--- src/store/userInfo.js
+++ src/store/userInfo.js
@@ -1,35 +1,27 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-import { API_PUBLIC_URL, COOLDOWN_TIME_IN_MS } from '../config';
+import { API_PUBLIC_URL } from '../config';
 
 export const useUserInfoStore = defineStore('userInfo', {
   state: () => ({
     likerIdMap: new Map(),
-    fetchTimestampMap: new Map(),
   }),
   getters: {
     likerId: (state) => (address) => state.likerIdMap.get(address),
   },
   actions: {
     async fetchLikerId(address) {
-      if (
-        !address
-        || this.likerIdMap.has(address)
-        || (
-          this.fetchTimestampMap.has(address)
-          && Date.now() - this.fetchTimestampMap.get(address) < COOLDOWN_TIME_IN_MS
-        )
-      ) return;
+      if (!address || this.likerIdMap.has(address)) return;
       try {
-        this.fetchTimestampMap.set(address, Date.now());
+        // set the key to prevent duplicate requests
+        this.likerIdMap.set(address, null);
         const { data } = await axios.get(`${API_PUBLIC_URL}/users/addr/${address}/min`);
         this.likerIdMap.set(address, data.user);
       } catch (error) {
-        if (error.response?.status === 404) {
-          this.likerIdMap.set(address, null);
-        } else {
-          this.fetchTimestampMap.delete(address);
+        if (error.response?.status !== 404) {
+          // remove the key to unblock future requests
+          this.likerIdMap.delete(address);
           this.fetchLikerId(address);
         }
       }
