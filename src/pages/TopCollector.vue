e8a7e66787f893b022d6aa422b10cf0893e6f566
William Chong <me@williamchong.cloud>
2023-02-02 16:38:13 +0800

    ✨ Export top collect list filtered by NFT class id

--- src/pages/TopCollector.vue
+++ src/pages/TopCollector.vue
@@ -1,5 +1,23 @@
 <template>
   <h2>Top NFT Collectors</h2>
+  <div>
+    <label>
+      Filter By NFT Class ID
+      <input
+        v-model="classIdInputString"
+        type="text"
+        size="40"
+      >
+    </label>
+    <button @click="getCollectorList">
+      Filter
+    </button>
+  </div>
+  <section>
+    <button @click="exportPageData">
+      Export result
+    </button>
+  </section>
   <table>
     <tr>
       <th>Address</th>
@@ -25,6 +43,7 @@ import UserLink from '../components/UserLink.vue';
 
 <script>
 import { indexerApi } from '../utils/proxy.js';
+import { downloadAsFile } from '../utils/util';
 
 const limit = 20;
 
@@ -33,12 +52,39 @@ export default {
   data() {
     return {
       owners: [],
+      classId: '',
+      classIdInputString: '',
     };
   },
   mounted() {
-    indexerApi.get('/statistics/nft/owners', { params: { limit } }).then((res) => {
+    this.getGlobalTopCollectorList();
+  },
+  methods: {
+    async getGlobalTopCollectorList() {
+      const res = await indexerApi.get('/statistics/nft/owners', { params: { limit } });
       this.owners = res.data.owners;
-    });
+    },
+    async getCollectorList() {
+      this.classId = this.classIdInputString;
+      if (!this.classId) {
+        await this.getGlobalTopCollectorList();
+        return;
+      }
+      const res = await indexerApi.get(`/likechain/likenft/v1/owner?class_id=${encodeURIComponent(this.classId)}`);
+      this.owners = res.data.owners.sort((a, b) => b.count - a.count);
+    },
+    exportPageData() {
+      if (this.$gtag) {
+        this.$gtag.event('export_collector_data', {
+          event_category: 'export',
+          event_label: 'collector',
+          value: 1,
+        });
+      }
+      const header = ['address, count'];
+      const contents = this.owners.map((o) => `${o.owner},${o.count}`);
+      downloadAsFile(header.concat(contents).join('\n'), `collector_${this.classId || 'global'}.csv`, 'text/csv');
+    },
   },
 };
 </script>
